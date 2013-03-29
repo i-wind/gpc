075beded1685463bf9f311a6a5c61ed17674103d
i.vetrov <ig-wind@mail.ru>
2013-03-30 03:28:59 +0400

    Pure ruby realization

--- pre-commit.rb
+++ pre-commit.rb
@@ -2,39 +2,38 @@
 # -*- coding: utf-8 -*-
 #@script   : pre-commit.rb
 #@created  : 2013-03-29 10:15
-#@changed  : 2013-03-30 01:36
-#@revision : 5
-#@about    :
+#@changed  : 2013-03-30 02:33
+#@revision : 6
+#@about    : git pre-commit hook to follow timestamp
+#            and revision of python and ruby scripts
 
 # git diff --cached --name-status
 # git diff --cached --name-only
 # git diff --cached --name-status | awk '$1 != "R" { print $2 }'
 # git diff --cached --name-only --diff-filter=ACM
 
+# file names in the current commit (except removed)
 fileNames = `git diff --cached --name-only --diff-filter=ACM`
 
 fileNames.split(/\n/).each() { |name|
-    if name =~ /(\.rb|\.py)$/
-        puts name
+    if name =~ /(\.rb|\.py)$/  # watching ruby & python scripts
+        script = nil  # current script text
+        File.open(name) {|f| script = f.read() }
         # change modification date
         time = Time.new
-        ret = time.strftime("%Y-%m-%d %H:%M")
-        `sed -i -r 's/(@changed\s*:\s+)[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}/\\1#{ret}/' #{name}`
+        now = time.strftime("%Y-%m-%d %H:%M")
+        result = script.gsub(/(@changed\s*:\s+)\d{4}-\d{2}-\d{2} \d{2}:\d{2}/) {|m| "%s%s" % [$1, now] }
         # change script revision
-        digit = `cat #{name} | grep -Po '@revision\\s*:\\s+\\d+' | grep -Po '\\d+'`
-        if digit
-            value = digit.to_i + 1
-            puts "Changing revision to #{value}"
-            `sed -i -r 's/(@revision\s*:\s+)[[:digit:]]+/\\1#{value}/' #{name}`
-        end
+        result = result.gsub(/(@revision\s*:\s+)(\d+)/) {|m| "%s%d" % [$1, $2.to_i + 1] }
+        # write back to script
+        File.open(name, 'w') {|f| f.write(result) }
         # add changes to commit
         `git add #{name}`
     end
 }
 
-# if exit status other then
-# 0 commit will fail
-# In an emergency the hook can be bypassed by passing –no-verify:
+# if exit status other then 0 commit will fail
+# in an emergency the hook can be bypassed by passing –no-verify:
 # $ git commit --no-verify ...
 
 exit(0)  # exit success
