7e977f43387901eb138e6ab62724c5a8bdde6b16
i.vetrov <ig-wind@mail.ru>
2013-03-31 18:55:42 +0400

    Some code refactoring

--- pre-commit.rb
+++ pre-commit.rb
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 #@script   : pre-commit.rb
 #@created  : 2013-03-29 10:15
-#@changed  : 2013-03-30 11:43
-#@revision : 8
+#@changed  : 2013-03-31 18:55
+#@revision : 9
 #@about    : git pre-commit hook to follow timestamp
 #            and revision of python and ruby scripts
 
@@ -15,13 +15,12 @@
 # file names in the current commit (except removed)
 fileNames = `git diff --cached --name-only --diff-filter=ACM`
 
-fileNames.split(/\n/).each() { |name|
+fileNames.split(/\n/).each { |name|
     if name =~ /(\.rb|\.py)$/  # watching ruby & python scripts
-        script = nil  # current script text
-        File.open(name) {|f| script = f.read() }
+        # current script text
+        script = File.read(name)
         # change modification date
-        time = Time.new
-        now = time.strftime("%Y-%m-%d %H:%M")
+        now = Time.new.strftime("%Y-%m-%d %H:%M")
         script.sub!(/(@changed\s*:\s+)\d{4}-\d{2}-\d{2} \d{2}:\d{2}/) {|m| $1 + now }
         # change script revision
         script.sub!(/(@revision\s*:\s+)(\d+)/) {|m| "%s%d" % [$1, $2.to_i + 1] }
