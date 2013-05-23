0ef5093dd4eff27b98e4687fde75a5e7a01087e4
i.vetrov <i.vetrov@inventos.ru>
2013-05-24 00:01:41 +0400

    ruby indentation

--- pre-commit.rb
+++ pre-commit.rb
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 #@script   : pre-commit.rb
 #@created  : 2013-03-29 10:15
-#@changed  : 2013-04-06 12:22
-#@revision : 10
+#@changed  : 2013-05-24 00:01
+#@revision : 11
 #@about    : git pre-commit hook to follow timestamp
 #            and revision of python and ruby scripts
 
@@ -16,21 +16,21 @@
 fileNames = `git diff --cached --name-only --diff-filter=ACM`
 
 fileNames.split(/\n/).each { |name|
-    if name =~ /(\.rb|\.py)$/  # watching ruby & python scripts
-        # current script text
-        script = File.read(name)
-        # change modification date
-        now = Time.new.strftime("%Y-%m-%d %H:%M")
-        script.sub!(/(@changed\s*:\s+)\d{4}-\d{2}-\d{2} \d{2}:\d{2}/) {|m| $1 + now }
-        # change script revision
-        script.sub!(/(@revision\s*:\s+)(\d+)/) {|m| "%s%d" % [$1, $2.to_i + 1] }
-        # change script version
-        script.sub!(/(__version__\s*=\s*\d+\.\d+\.)(\d+)/) {|m| "%s%d" % [$1, $2.to_i + 1] }
-        # write back to script
-        File.open(name, 'w') {|f| f.write(script) }
-        # add changes to commit
-        `git add #{name}`
-    end
+  if name =~ /(\.rb|\.py)$/  # watching ruby & python scripts
+    # current script text
+    script = File.read(name)
+    # change modification date
+    now = Time.new.strftime("%Y-%m-%d %H:%M")
+    script.sub!(/(@changed\s*:\s+)\d{4}-\d{2}-\d{2} \d{2}:\d{2}/) {|m| $1 + now }
+    # change script revision
+    script.sub!(/(@revision\s*:\s+)(\d+)/) {|m| "%s%d" % [$1, $2.to_i + 1] }
+    # change script version
+    script.sub!(/(__version__\s*=\s*\d+\.\d+\.)(\d+)/) {|m| "%s%d" % [$1, $2.to_i + 1] }
+    # write back to script
+    File.open(name, 'w') {|f| f.write(script) }
+    # add changes to commit
+    `git add #{name}`
+  end
 }
 
 # if exit status other then 0 commit will fail
