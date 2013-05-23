22eacad3f3f412226d7354fb737466862c440c92
i.vetrov <i.vetrov@inventos.ru>
2013-05-24 02:36:34 +0400

    Using lambda in re.sub

--- pre-commit.py
+++ pre-commit.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 #@script   : pre-commit.py
 #@created  : 2013-03-30 00:15
-#@changed  : 2013-05-24 02:08
-#@revision : 3
+#@changed  : 2013-05-24 02:36
+#@revision : 4
 #@about    : git pre-commit hook to follow timestamp
 #            and revision of python and ruby scripts
 
@@ -30,22 +30,21 @@ if __name__ == '__main__':
     modified = re.compile('^[ACM]+\s+(?P<name>.*\.py)', re.MULTILINE)
     files = modified.findall( system('git', 'status', '--porcelain') )
 
-    def repl(m):
-        return m.group(1) + now()
-
-    def vers(m):
-        return m.group(1) + str(int(m.group(2))+1)
-
     for name in files:
         # current script text
         with open(name, 'r') as fd: script = fd.read()
         # change modification date
-        script = re.sub('(@changed\s*:\s+)\d{4}-\d{2}-\d{2} \d{2}:\d{2}', repl, script)
+        script = re.sub('(@changed\s*:\s+)\d{4}-\d{2}-\d{2} \d{2}:\d{2}',
+                        lambda m: m.group(1) + now(), script)
         # change script revision
-        script = re.sub('(@revision\s*:\s+)(\d+)', vers, script)
+        script = re.sub('(@revision\s*:\s+)(\d+)',
+                        lambda m: m.group(1) + str(int(m.group(2))+1), script)
         # change script version
-        script = re.sub('(__version__\s*=\s*\d+\.\d+\.)(\d+)', vers, script)
+        script = re.sub('(__version__\s*=\s*\d+\.\d+\.)(\d+)',
+                        lambda m: m.group(1) + str(int(m.group(2))+1), script)
         # write back to script
         with open(name, 'w') as fd: fd.write(script)
         # add changes to commit
         system('git', 'add', name)
+
+    sys.exit(0)
