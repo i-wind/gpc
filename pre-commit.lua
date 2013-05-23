d8d1142220ee7eeb005dfb76e6fb88e0f22ef125
i.vetrov <i.vetrov@inventos.ru>
2013-05-24 02:19:27 +0400

    pre-commit git hook in lua

--- pre-commit.lua
+++ pre-commit.lua
@@ -3,11 +3,19 @@
 --[[
  @script   : pre-commit.lua
  @created  : 2013-05-18 01:47
- @changed  : 2013-05-18 03:09
- @revision : 2
+ @changed  : 2013-05-18 12:57
+ @revision : 3
  @about    : git pre-commit hook to follow script's
              timestamp and revision
 ]]
+
+-- split string
+function string:split(sep)
+    local sep, fields = sep or ":", {}
+    local pattern = string.format("([^%s]+)", sep)
+    self:gsub(pattern, function(c) fields[#fields+1] = c end)
+    return fields
+end
 
 --[[
 git diff --cached --name-status
@@ -16,34 +24,36 @@ git diff --cached --name-status | awk '$1 != "R" { print $2 }'
 git diff --cached --name-only --diff-filter=ACM
 ]]
 
---[[
 -- file names in the current commit (except removed)
-local f = io.popen('git diff --cached --name-only --diff-filter=ACM', 'r')
-local file_names = f:read '*a'
-f:close()
+local pipe = io.popen('git diff --cached --name-only --diff-filter=ACM', 'r')
+local file_names = pipe:read '*a'
+pipe:close()
 
-if file_names ~= "" then
-    print(file_names)
-else
-    print("empty...")
+for num, file_name in ipairs(file_names:split("\n")) do
+
+    if file_name ~= "" then
+        local f = io.open(file_name, "r")
+        local script = f:read("*all")
+        f:close()
+
+        -- replace change date
+        local text, dummy = string.match(script, "(@changed%s*:%s+)(%d+-%d+-%d+ %d+:%d+)")
+        local now = os.date("%Y-%m-%d %H:%M")
+        script = string.gsub(script, "(@changed%s*:%s+)(%d+-%d+-%d+ %d+:%d+)",
+                             string.format("%s%s", text, now))
+        -- increment revision
+        local text, rev = string.match(script, "(@revision%s*:%s+)(%d+)")
+        local revision = tonumber(rev) + 1
+        script = string.gsub(script, "(@revision%s*:%s+)(%d+)",
+                             string.format("%s%d", text, revision))
+
+        f = io.open(file_name, "w")
+        f:write(script)
+        f:close()
+
+        os.execute('git add ' .. file_name)
+    end
+
 end
-]]
-
-local f = io.open("pre-commit.lua", "r")
-local script = f:read("*all")
-f:close()
-
--- replace change date
-local text, dummy = string.match(script, "(@changed%s*:%s+)(%d+-%d+-%d+ %d+:%d+)")
-local now = os.date("%Y-%m-%d %H:%M")
-script = string.gsub(script, "(@changed%s*:%s+)(%d+-%d+-%d+ %d+:%d+)",
-                     string.format("%s%s", text, now))
--- increment revision
-local text, rev = string.match(script, "(@revision%s*:%s+)(%d+)")
-local revision = tonumber(rev) + 1
-script = string.gsub(script, "(@revision%s*:%s+)(%d+)",
-                     string.format("%s%d", text, revision))
-
-print(script)
 
 os.exit(0)
