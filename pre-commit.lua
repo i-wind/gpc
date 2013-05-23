fd560dc44262ad7535cf1cccc9b2e997c6af09c1
i.vetrov <i.vetrov@inventos.ru>
2013-05-24 02:12:28 +0400

    Template for pre-commit git hook in lua 5.2

---
+++ pre-commit.lua
@@ -0,0 +1,12 @@
+#!/usr/bin/env lua
+-- encoding: utf-8
+--[[
+ @script   : pre-commit.lua
+ @created  : 2013-05-18 01:47
+ @changed  : 2013-05-18 01:47
+ @revision : 0
+ @about    : git pre-commit hook to follow script's
+             timestamp and revision
+]]
+
+os.exit(0)
